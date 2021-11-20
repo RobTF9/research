11904c571385f6617fc1651ccc83f7dcaf1b87b2
Rob <robtf9@icloud.com>
2021-11-20 14:27:21 +0000

    chore: remove swear word

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -2,7 +2,7 @@ import nc from 'next-connect'
 import type { NextApiResponse } from 'next'
 
 const handler = nc().get((req, res: NextApiResponse) => {
-  res.status(200).json({ message: 'You cunt' })
+  res.status(200).json({ message: 'Hello' })
 })
 
 export default handler
